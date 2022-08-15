6defa625ec4c108d67c9f86fe3960e1c8975a6f4
David <david.njoroge@student.moringaschool.com>
2022-08-15 22:19:18 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
+let num1=31;
+let num2=2;
+let multiply = num1*num2
+
+let random = Math.floor(Math.random() * 10)
+
+
+let num3 = 16
+let num4 = 6
+
+let mod = num3 % num4
+
+let max =20
+
 // Write your code here
